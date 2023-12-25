5835cbfe03e3dede50eb4df6d567e0234d53c963
Rishab Das <das.rishabk@gmail.com>
2023-12-24 20:44:56 -0500

    Initial Commit

---
+++ Dr.Habit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dr.Habit
+//
+//  Created by RKD on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dr.Habit/Dr_HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dr_HabitApp.swift
+//  Dr.Habit
+//
+//  Created by RKD on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Dr_HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
