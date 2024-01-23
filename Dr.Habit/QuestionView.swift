75c6537ed2a7e488c22d3a1e8b7d85c6628ff331
Rishab Das <das.rishabk@gmail.com>
2024-01-22 21:58:06 -0500

    added question answer hstack

--- Dr.Habit/QuestionView.swift
+++ Dr.Habit/QuestionView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct QuestionView: View {
+    @State private var name: String = ""
+    @State private var age: String = ""
+    @State private var dob: String = ""
     
-    @State public var name: String = ""
-    @State public var age: Int;
-    @State public var dob: String = ""
+    
     
     var body: some View {
         NavigationView {
@@ -19,37 +20,87 @@ struct QuestionView: View {
                 LinearGradient(gradient: Gradient(colors: [.myPurple, .black]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .ignoresSafeArea()
                     .overlay {
-                        Text("Name")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(.white)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                        InputFieldView(data: $name, title: "Name")
-                        Text("Age")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(.white)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                        Text("Date Of Birth")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            .foregroundColor(.white)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        HStack {
+                            Text("Name")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .position(x:60, y:50)
+                            TextField("Name", text: $name)
+                                .padding(.horizontal, 10)
+                                .frame(height: 42)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .overlay(  // add an overlay
+                                    RoundedRectangle(cornerSize: CGSize(width: 60, height: 60))
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                                .frame(width: 250, height: 100)
+                                .position(x:50, y:50)
+                            
+                        }
+                        HStack {
+                            Text("Age")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .position(x:60, y:130)
+                            TextField("Age", text: $age)
+                                .padding(.horizontal, 10)
+                                .frame(height: 42)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .overlay(  // add an overlay
+                                    RoundedRectangle(cornerSize: CGSize(width: 60, height: 60))
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                                .frame(width: 250, height: 100)
+                                .position(x:50, y:130)
+                        }
+                        HStack {
+                            Text("DOB")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .position(x:60, y:210)
+                            TextField("Date Of Birth", text: $dob)
+                                .padding(.horizontal, 10)
+                                .frame(height: 42)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .overlay(  // add an overlay
+                                    RoundedRectangle(cornerSize: CGSize(width: 60, height: 60))
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                                .frame(width: 250, height: 100)
+                                .position(x:50, y:210)
+                        }
                         Text("Gender")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .font(.title)
                             .foregroundColor(.white)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.bold)
+                            .padding(.bottom, 40)
                         Text("Height")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .font(.title)
                             .foregroundColor(.white)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.bold)
+                            .padding(.bottom, -30)
                         Text("Weight")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .font(.title)
                             .foregroundColor(.white)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.bold)
+                            .padding(.top, 100)
                     }
             }
         }
     }
 }
 
-#Preview {
-    QuestionView(name: $name)
+struct QuestionView_Previews: PreviewProvider {
+    @State static var name: String = ""
+    
+    static var previews: some View {
+        QuestionView()
+    }
 }
+
