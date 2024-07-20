49d7150998c54dbff4fe2948340c813e89a336fe
Rishab Das <das.rishabk@gmail.com>
2024-07-19 21:49:01 -0400

    finished overlay

--- Dr.Habit/AddView.swift
+++ Dr.Habit/AddView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct AddView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            Color("myBackgroundColor")
+                .ignoresSafeArea()
+                .overlay {
+                    
+                }
+        }
     }
 }
 
